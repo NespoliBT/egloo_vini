20d063fa37219055c72f8d1496703b53bf4a4230
NespoliBT <40525058+NespoliBT@users.noreply.github.com>
2025-09-10 14:06:07 +0000

    mobile!

--- src/lib/components/Nav/Nav.svelte
+++ src/lib/components/Nav/Nav.svelte
@@ -1,9 +1,12 @@
 <script lang="ts">
   import menu from "$lib/data/menu";
   import NavButtons from "$lib/components/NavButtons/NavButtons.svelte";
+  import { fly } from "svelte/transition";
+
+  let mobileMenuOpen = false;
 </script>
 
-<nav>
+<nav class="desktop">
   <ul class="left">
     <NavButtons menu={menu.left} />
   </ul>
@@ -16,6 +19,24 @@
     <NavButtons menu={menu.right} />
   </ul>
 </nav>
+{#if mobileMenuOpen}
+  <nav class="mobile" transition:fly={{ y: -100, duration: 300 }}>
+    <div class="top">
+      <img class="logo" src="assets/logo.png" alt="" />
+    </div>
+
+    <ul class="mobileMenu">
+      <NavButtons menu={[...menu.left, ...menu.right]} />
+    </ul>
+  </nav>
+{/if}
+
+<button
+  class="mobileToggle"
+  on:click={() => (mobileMenuOpen = !mobileMenuOpen)}
+>
+  <div class="icon"></div>
+</button>
 
 <style lang="scss">
   @use "nav";
